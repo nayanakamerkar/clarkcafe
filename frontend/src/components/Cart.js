20a39fbc21aebf16af9eff43f13091b4486a208e
Yash M Naik <yash.naik1@gmail.com>
2023-10-22 01:19:25 -0400

    Yash Changes This time I know its the cart

--- frontend/src/components/Cart.js
+++ frontend/src/components/Cart.js
@@ -10,10 +10,29 @@ const Cart = () => {
         removeItem,
     } = useCart();
 
+    if (isEmpty) return <p>Your Cart is empty</p>
 
     return (
         <div>
-
+            <h1>Your Cart ({totalUniqueItems})</h1>
+            <ul>
+                {items.map((item) => (
+                    <li key={item.id}>
+                        {item.quantity} x {item.name} &mdash;
+                        <button
+                            onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+                        >
+                            -
+                        </button>
+                        <button
+                            onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+                        >
+                            +
+                        </button>
+                        <button onClick={() => removeItem(item.id)}>&times;</button>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
